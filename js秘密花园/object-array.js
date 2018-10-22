05b1ef528bd093cdcc1ae819a25d4584fc178eb3
fatdoge <704274491@qq.com>
2018-10-22 15:17:05 +0800

    object-array

---
+++ js秘密花园/object-array.js
@@ -0,0 +1,93 @@
+// let action={
+//   type:'GREETING',
+//   payload:{
+//     msg:'hello,world'
+//   }
+// }
+// const add = (state, { payload: todo }) => {
+//   return state.concat(todo);
+// };
+// let state=[1]
+// console.log(add(state,action))
+let initialState= {
+  products: [
+    { name: 'dva', id: 1, key: 1 },
+    { name: 'antd', id: 2, key: 2 },
+    { name: 'fatdoge', id: 3, key: 3 },
+    { name: 'bilibili', id: 4, key: 4 },
+
+  ]
+}
+const add=({products}) => (
+  {products}
+)
+console.log(add(initialState))
+
+// function *generatorDemo(){
+//   yield 1
+//   yield 2
+// }
+// let it=generatorDemo()
+// console.log(it.next())
+// console.log(it.next())
+// console.log(it.next())
+
+// 正常异步执行
+// function demo() {
+//   new Promise(function (resolve, reject) {
+//     resolve(3)
+//   }).then((value) => {
+//     console.log(value)
+//   })
+//   console.log(4)
+// }
+// demo();         // 执行结果依次是：4，3
+
+// // 生成器异步转同步
+// let gen = null;
+// function* genDemo() {
+//   yield setTimeout(() => {
+//     console.log(3);
+//     gen.next();
+//   }, 100)
+//   console.log(4)
+// }
+// gen = genDemo();
+// gen.next()  // // 执行结果依次是：3，4
+
+// // async...await...异步转同步
+// async function asyncDemo() {
+//   await new Promise(function (resolve, reject) {
+//     resolve(3)
+//   }).then((value) => {
+//     console.log(value)
+//   })
+//   console.log(4)
+// }
+// asyncDemo();    // 执行结果依次是：3，4
+
+// function* ticketGenerator() {
+//   for (var i = 0; true; i++)
+//     yield i; //一定要打分号
+// }
+// var takeANumber = ticketGenerator()
+// console.log(takeANumber.next().value) //{value: 1, done: false}
+// console.log(takeANumber.next().value) //{value: 2, done: false}
+// console.log(takeANumber.next().value) //{value: 3, done: false}
+// console.log(takeANumber.next().value) //{value: 4, done: false}
+// console.log(takeANumber.next().value) //{value: 4, done: false}
+
+// function *fab(max) {
+//   var count = 0, last = 0, current = 1;
+
+//   while (count++ < max) {
+//     yield current;
+//     var tmp = current;
+//     current += last;
+//     last = tmp;
+//   }
+// }
+
+// for (var i of fab(10)) {
+//   console.log(i);
+// }
--- 你不知道的js上卷/EventHandle.js
+++ 你不知道的js上卷/EventHandle.js
@@ -1,6 +1,18 @@
-function generators(){
+function *generators(){
     for(var i=0;i<10;i++){
         yield i*2;
     }
 }
 console.log(generators());
+let it=generators()
+console.log(it.next())
+console.log(it.next())
+console.log(it.next())
+console.log(it.next())
+console.log(it.next())
+console.log(it.next())
+console.log(it.next())
+console.log(it.next())
+console.log(it.next())
+console.log(it.next())
+console.log(it.next())
